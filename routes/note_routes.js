ccec1b5bc169086909ae269fbc4b750523cdfcb7
destimon <destaom@gmail.com>
2017-10-14 19:49:18 +0300

    READ+UPDATE+DELETE funcs for DB

--- routes/note_routes.js
+++ routes/note_routes.js
@@ -1,15 +1,65 @@
 
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+
+	// READ
+
+	app.get('/notes/:id', (req, res) => {
+		const id = req.params.id;
+		const details = {'_id': new ObjectID(id) };
+
+		db.collection('notes').findOne(details, (err, item) => {
+			if (err) {
+				res.send(' Pizda, oshibka ');
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
+	// CREATE
+
 	app.post('/notes', (req, res) => {
-		const Note = { text: req.body.body, title: req.body.title };
-		db.collection('notes').insert(Note, (err, result) => {
+		const note = { text: req.body.body, title: req.body.title };
+		db.collection('notes').insert(note, (err, result) => {
 			if (err) {
 				res.send({ 'error': 'Error epta'});
-			}
-			else {
+			} else {
 				res.send(result.ops[0]);
 			}
 		});
 	});
 
+	// DELETE
+
+	app.delete('/notes/:id', (req, res) => {
+		const id = req.params.id;
+		const details = {'_id': new ObjectID(id) };
+
+		db.collection('notes').remove(details, (err, item) => {
+			if (err) {
+				res.send(' Pizda, oshibka ');
+			}
+			else {
+				res.send('Note ' + id + ' deleted');
+			}
+		});
+	});
+
+	// UPDATE
+
+	app.put('notes/:id', (req, res) => {
+		const id = req.params.id;
+		const details = {'_id':new ObjectID(id) };
+		const note = { text: req.req.body, title: req.body.title };
+
+		db.collectionO('notes').update(details, note, (err, result) => {
+			if (err) {
+				res.send(' Pizda, oshibka ');	
+			} else {
+				res.send(note);
+			}
+		});
+	});
 };
