d6b17f8411bd5c16f4aa8d20b7cc11a85e51cceb
destimon <destaom@gmail.com>
2017-11-26 04:06:10 +0200

    AJAX SendRequest() function writed

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+// Create Request
 
 function CreateRequest() {
 	var Request = false;
@@ -8,10 +9,10 @@ function CreateRequest() {
 	} else if (ActiveXObject) {
 		// IE
 		try {
-			Request = new ActiveXObject("Microsoft.XMLHTTP");
+			Request = new ActiveXObject('Microsoft.XMLHTTP');
 		}
     catch(CatchException) {
-      Request = new ActiveXObject("Msxm12.XMLHTTP");
+      Request = new ActiveXObject('Msxm12.XMLHTTP');
     }
 	}
 
@@ -20,4 +21,33 @@ function CreateRequest() {
   }
 
   return Request;
+}
+
+// Send Request
+
+function SendRequest(r_method, r_path, r_args, r_handler) {
+  var Request = CreateRequest();
+
+  if (!Request) return;
+
+  Request.onreadystatechange = function() {
+    if (Request.readyState == 4) 
+      r_handler(Request);
+  }
+
+  // is there get?
+  if (r_method.toLowerCase() == 'get' && r_args.length > 0) 
+    r_path += '?' + r_args;
+
+  // Init connect
+  Request.open(r_method, r_path, true);
+
+  if (r.method.toLowerCase() == "post") {
+    // POST
+    Request.setRequestHeader('ContentType', 'application/x-www-form-urlencoded; charset=utf8');
+    Request.send(r_args);
+  } else {
+    // GET
+    Request.send(null);
+  }
 }
--- routes/controller.js
+++ routes/controller.js
@@ -9,14 +9,14 @@ let path = require('path');
 
 module.exports = function(app, passport, db) {
 
-	// HOME =================================
-
+	// HOME ============================================================================
+  // =================================================================================
 	app.get('/', isLoggedIn, (req, res) => {
 		res.redirect('me');
 	});
 
-	// LOGIN =================================
-
+	// LOGIN ============================================================================
+  // ==================================================================================
 	app.get('/login', (req, res) => {
 		if (req.isAuthenticated())
 	      res.redirect('/me');
@@ -26,41 +26,40 @@ module.exports = function(app, passport, db) {
 
 	app.post('/login', passport.authenticate('local-login', {
 		successRedirect : '/me',
-		failureRedirect : '/login',
-		failureFlash: true
-	}));
+    failureRedirect : '/login',
+    failureFlash: true
+  }));
 
-	// SIGNUP =================================
+  // SIGNUP ============================================================================
+  // ===================================================================================
+  app.get('/signup', (req,res) => {   
+    let empty = new User();
 
-	app.get('/signup', (req,res) => {
-		
-		let empty = new User();
+    // To avoid problem with undefined local
+    empty.login = '';
+    empty.password = '';
+    empty.name = '';
+    empty.surname = '';
+    empty.dob = '';
+    empty.gender = '';
 
-		// To avoid problem with undefined local
-		empty.login = '';
-		empty.password = '';
-		empty.name = '';
-		empty.surname = '';
-		empty.dob = '';
-		empty.gender = '';
+    res.render('signup.ejs', { 
+      message: req.flash('signupMessage'),
+      mydata:  empty
+    });
+  
+  });
 
-		res.render('signup.ejs', { 
-			message: req.flash('signupMessage'),
-			mydata:  empty
-		});
-	
-	});
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/me',
+    failureRedirect : '/signup',
+    failureFlash  : true
+  }));
 
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/me',
-		failureRedirect : '/signup',
-		failureFlash	: true
-	}));
-
-	// USERS ================================
-
+  // USERS ============================================================================
+  // ==================================================================================
     app.get('/me', isLoggedIn, function(req, res) {
-			console.log(req.user);
+      console.log(req.user);
 
         res.render('account.ejs', {
           user : req.user, // get the user out of session and pass to template
@@ -69,218 +68,192 @@ module.exports = function(app, passport, db) {
     });
 
     app.get('/usr/:login', (req, res) => {
-    	// req.isAuthenticated();
-    	
-    	let login = req.params.login;
-    	let current = req.user;
-		User.findOne( {'login' : login }, (err, getuser) => {
-			
-			res.render('account.ejs', {
-				user : getuser,
-				mydata : current
-			});			
-		});		
-	});
+      // req.isAuthenticated();
+      
+      let login = req.params.login;
+      let current = req.user;
+    User.findOne( {'login' : login }, (err, getuser) => {
+      
+      res.render('account.ejs', {
+        user : getuser,
+        mydata : current
+      });     
+    });   
+  });
 
 
   app.get('/usr/:login/avatar', (req, res) => {
+    try {
+      User.findOne( {'login' : req.params.login }, function (err, user) {
+        if (err) {
+          res.send(err);
+        } else if (user.avatar.default == undefined || user.avatar.default == true) {
+          res.sendfile(path.join(__dirname, '../public/img/no_avatar.jpg'));
+        } else {
+          res.setHeader('Cache-Control', 'public, max-age=3000000');
+          // res.contentType(user.avatar.contentType);
+          res.send(user.avatar.data);
+          console.log('AVATAR:' + user.avatar);
+          console.log('DEFAULT: ' + user.avatar.default);
+        }
+      });
+    } catch (err) {
+      res.send(err);
+    }
+  });
 
-	  try {
-	    User.findOne( {'login' : req.params.login }, function (err, user) {
-	      
-	      if (err) {
-	        res.send(err);
-	      } else if (user.avatar.default == undefined || user.avatar.default == true) {
-	        res.sendfile(path.join(__dirname, '../public/img/no_avatar.jpg'));
-	      } else {
-	        res.setHeader('Cache-Control', 'public, max-age=3000000');
-	        // res.contentType(user.avatar.contentType);
-	        res.send(user.avatar.data);
-	        console.log('AVATAR:' + user.avatar);
-	        console.log('DEFAULT: ' + user.avatar.default);
-	      }
-	    });
+  // ADD ============================================================================
+  // ================================================================================
+  app.post('/add?usr=:login', isLoggedIn, function(req, res) {
+    let login = req.params.login;
+    
+    
+  });
 
-	  } catch (err) {
-	    res.send(err);
-	  }
-	});
+  // CONTACTS ============================================================================
+  // =====================================================================================
+  app.get('/contacts/:p', isLoggedIn, (req, res) => {
+    let pg = req.params.p;
 
-  
-	// LOGOUT =====user.local.login============================
+    User.paginate({ }, { page: pg, limit: 3}, (err, data) => {
+      console.log('page: ' + data.page);
+      console.log('pages: ' + data.pages);
 
-	app.get('/logout', (req, res) => {
-		req.logout();
-		res.redirect('/');
-	});
-	
+      res.render('contacts.ejs', {
+        pgs: data.pages,
+        curr: data.page,
+        users: data.docs,
+        mydata: req.user
+      });
+    });
+  });
 
-	// CONTACTS ================================
+  app.post('/contacts', function(req, res) {    
+    // Get user.id
+    let targetLogin = req.body.addusr;
+    console.log(targetLogin);
 
-	app.get('/contacts/:p', isLoggedIn, (req, res) => {
+    // Get curr.user.id
+    Contacts.findOne( {'secondLogin' : targetLogin }, (err, cont) => {
+      if (cont) {
+    
+        if( cont.firstStatus == true) {
+        cont.firstStatus = false;
+        req.flash('ContactMessage', 'Вы успешно отозвали заявку!'); 
+        }
+        else {
+          cont.firstStatus = true;
+          req.flash('ContactMessage', 'Вы отправили заявку!');  
+        }
 
-	  let pg = req.params.p;
+        
+        cont.save(function(err){
+          if (err) throw err;
+        });
 
-	  User.paginate({ }, { page: pg, limit: 3}, (err, data) => {
+      } else {
 
-	  	console.log('page: ' + data.page);
-	  	console.log('pages: ' + data.pages);
+        // Set FirstStatus ON
+        var newContacts = new Contacts();
 
-	  	res.render('contacts.ejs', {
-	  		pgs: data.pages,
-	  		curr: data.page,
-	  		users: data.docs,
-	  		mydata: req.user
-	  	});
-	  });
-	});
+        newContacts.firstLogin = req.user.local.login;
+        newContacts.secondLogin = targetLogin;
+        newContacts.firstStatus = true; 
+      
+        newContacts.save(function(err){
+          if (err) throw err;
+        }); 
+        req.flash('ContactMessage', 'Вы отправили заявку!');
+      }
+        res.redirect('contacts/');
+    });
+  });
 
+  // EDIT ============================================================================
+  // =================================================================================
+  app.get('/edit', isLoggedIn, function(req,res) {
+    let id = req.user.id;
 
-	app.post('/contacts', function(req, res) {
-		
-		// Get user.id
-		let targetLogin = req.body.addusr;
-		console.log(targetLogin);
+    res.render('edit', { 
+      mydata: req.user,       
+      message: req.flash('signupMessage', 'я даун')
+    });   
+  });
 
-		// Get curr.user.id
-		
-		
-		Contacts.findOne( {'secondLogin' : targetLogin }, (err, cont) => {
-			if (cont) {
-		
-				if( cont.firstStatus == true) {
-				cont.firstStatus = false;
-				req.flash('ContactMessage', 'Вы успешно отозвали заявку!');	
-				}
-				else {
-					cont.firstStatus = true;
-					req.flash('ContactMessage', 'Вы отправили заявку!');	
-				}
-
-				
-				cont.save(function(err){
-					if (err) throw err;
-				});
-
-			} else {
-
-				// Set FirstStatus ON
-				var newContacts = new Contacts();
-
-				newContacts.firstLogin = req.user.local.login;
-				newContacts.secondLogin = targetLogin;
-				newContacts.firstStatus = true;	
-			
-				newContacts.save(function(err){
-					if (err) throw err;
-				});	
-				req.flash('ContactMessage', 'Вы отправили заявку!');
-			}
-				res.redirect('contacts/');
-		});
-	});
-
-
-
-
-	// EDIT
-
-	app.get('/edit', isLoggedIn, function(req,res) {
-
-		let id = req.user.id;
-
-
-		res.render('edit', { 
-			mydata: req.user,				
-			message: req.flash('signupMessage', 'я даун')
-		});
-		
-	});
-
-	app.post('/edit', function(req, res) {
-
-		let id = req.user.id;
-
-		let login = req.body.login;
-		let name = req.body.name;
-		let surname = req.body.surname;
-		let dob		= req.body.dob;
-		let gender	= req.body.gender;		 
-		let about = req.body.about;
-		
-		User.findById(id, function(err, user) {
-
-			user.login = login;
-			
-			user.name = name;
-			user.surname = surname;
-			user.dob = dob;
-			user.gender = gender;
-			user.about = about;
-			
-			console.log(req.files);
-			if (!req.files.avatar) {
-				user.avatar.default = true;
-			} else {
-				user.avatar = {
-					data: req.files.avatar.data,
-					contentType: req.files.avatar.mimetype,
-					default: false
-				}
-			}
+  app.post('/edit', function(req, res) {
+    let id = req.user.id;
+    let login = req.body.login;
+    let name = req.body.name;
+    let surname = req.body.surname;
+    let dob   = req.body.dob;
+    let gender  = req.body.gender;     
+    let about = req.body.about;
+    
+    User.findById(id, function(err, user) {
+      user.login = login;
+      user.name = name;
+      user.surname = surname;
+      user.dob = dob;
+      user.gender = gender;
+      user.about = about;
+      
+      console.log(req.files);
+      if (!req.files.avatar) {
+        user.avatar.default = true;
+      } else {
+        user.avatar = {
+          data: req.files.avatar.data,
+          contentType: req.files.avatar.mimetype,
+          default: false
+        }
+      }
 
       console.log('user :');
       console.log(user);
       console.log('req.files.avatar :');
       console.log(req.files.avatar);
 
-			user.save(function(err) {
-				if (err) throw err;
-			});
-		
-			res.redirect('edit');
-		});
-
-
-	});
-
-	// HELP ============================================================================
-	// =================================================================================
-
-	app.get('/help', function(req, res) {
-		res.render('devinfo', {
-			mydata: req.user
+      user.save(function(err) {
+        if (err) throw err;
+      });
+      res.redirect('edit');
     });
+
+
+  });
+
+  // HELP ============================================================================
+  // =================================================================================
+  app.get('/help', function(req, res) {
+    res.render('devinfo', {
+      mydata: req.user
+    });
+  });
+
+  // FEED ============================================================================
+  // =================================================================================
+  app.get('/feed', isLoggedIn, function(req,res) {
+    res.render('feed', {
+      mydata: req.user
+    });
+  
+  });
+	// LOGOUT ==============================================================================
+  // =====================================================================================
+	app.get('/logout', (req, res) => {
+		req.logout();
+		res.redirect('/');
 	});
 
-	// FEED ============================================================================
-	// =================================================================================
-	app.get('/feed', isLoggedIn, function(req,res) {
-		res.render('feed', {
-			mydata: req.user
-		});
-	});
 
-	// ADD ============================================================================
-	// ================================================================================
-	app.post('/add?usr=:login', isLoggedIn, function(req, res) {
-    let login = req.params.login;
-    
-	});
+  // isLoggedIn  ============================================================================
+  function isLoggedIn(req, res, next) {
 
+      // if user is authenticated in the session, carry on 
+      if (req.isAuthenticated())
+          return next();
 
-	// isLoggedIn  ============================================================================
-	function isLoggedIn(req, res, next) {
-
-	    // if user is authenticated in the session, carry on 
-	    if (req.isAuthenticated())
-	        return next();
-
-	    // if they aren't redirect them to the home page
-	    res.redirect('/login');
-	}
-
-
-
-
+      // if they aren't redirect them to the home page
+      res.redirect('/login');
+  }
 };
